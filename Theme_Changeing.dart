f7570c44f8658273b96f4f90039ab974ed856381
anivkuttan <69431047+anivkuttan@users.noreply.github.com>
2022-08-08 23:23:00 +0530

    Update Theme_Changeing.dart

--- Theme_Changeing.dart
+++ Theme_Changeing.dart
@@ -55,34 +55,48 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
+    var style = Theme.of(context);
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.light,
       theme: MyThemeDataClass.getCurrentTheme,
+      darkTheme: MyThemeDataClass.darkTheme,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Dark and Light Themeing'),
           actions: [
             Switch(
-                value:MyThemeDataClass.isDarkMode,
+                value: MyThemeDataClass.isDarkMode,
                 onChanged: (switchvalue) {
                   setState(() {
-                   MyThemeDataClass.isDarkMode = switchvalue;
+                    MyThemeDataClass.isDarkMode = switchvalue;
                   });
                 })
           ],
         ),
-        body: const Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(fontSize: 30),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+             const  Text(
+                'Hello World',
+                // style: style.textTheme.headline3,
+              ),
+              ElevatedButton(
+                child: const Text('Click me'),
+                onPressed: () {},
+              ),
+              const TextField(),
+            ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(onPressed: () {},),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {},
+        ),
       ),
     );
   }
@@ -92,17 +106,33 @@ class MyThemeDataClass {
   static bool isDarkMode = false;
   static get lightTheme {
     return ThemeData(
+      brightness: Brightness.light,
       colorScheme: const ColorScheme.light(),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(23),
+        ),
+      ),
+      //  buttonTheme: ButtonThemeData(
+
+      //  ),
     );
   }
 
   static get darkTheme {
     return ThemeData(
+      brightness: Brightness.dark,
       colorScheme: const ColorScheme.dark(),
+      scaffoldBackgroundColor: const Color.fromARGB(255, 29, 88, 58),
+      inputDecorationTheme: InputDecorationTheme(
+        border: InputBorder.none,
+        filled: true,
+        fillColor: Colors.yellow[200],
+      ),
     );
   }
 
-  static  get getCurrentTheme => isDarkMode ?darkTheme:lightTheme ;
-
+  static get getCurrentTheme => isDarkMode ? darkTheme : lightTheme;
 }
+
 
